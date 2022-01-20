e706bcf3c17f2fbcba28210bd184cdabad2666b6
BuXeMond <61242682+BuXeVi1@users.noreply.github.com>
2022-01-20 13:24:19 +0700

    This is my code

---
+++ F2_but_F/F2_but_F.ino
@@ -0,0 +1,101 @@
+//Arkkhanirut Pandej No.3 M.5/5
+
+const int trigPin = 12;
+const int echoPin = 13;
+
+const int LED1 = A0;
+const int LED2 = A1;
+const int LED3 = A2;
+const int LED4 = A3;
+const int LED5 = A4;
+const int LED6 = A5;
+const int LED7 = 2;
+
+int duration = 0;
+int distance = 0;
+
+void setup() 
+{
+  pinMode(trigPin , OUTPUT);
+  pinMode(echoPin , INPUT);
+  
+  pinMode(LED1 , OUTPUT);
+  pinMode(LED2 , OUTPUT);
+  pinMode(LED3 , OUTPUT);
+  pinMode(LED4 , OUTPUT);
+  pinMode(LED5 , OUTPUT);
+  pinMode(LED6 , OUTPUT);
+  pinMode(LED7 , OUTPUT);
+  
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+ digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);  
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration/58.2;
+
+  if ( distance <= 50 )
+  {
+    digitalWrite(LED1, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED1, LOW);
+  }
+  if ( distance <= 75 )
+  {
+    digitalWrite(LED2, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED2, LOW);
+  }
+  if ( distance <= 100 )
+  {
+    digitalWrite(LED3, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED3, LOW);
+  }
+  if ( distance <= 125 )
+  {
+    digitalWrite(LED4, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED4, LOW);
+  }
+  if ( distance <= 150 )
+  {
+    digitalWrite(LED5, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED5, LOW);
+  }
+  if ( distance <= 175 )
+  {
+    digitalWrite(LED6, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED6, LOW);
+  }
+  if ( distance <= 200 )
+  {
+    digitalWrite(LED7, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED7, LOW);
+  }
+  delay(100);
+}
